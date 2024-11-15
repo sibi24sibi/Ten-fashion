f7c5df3692adbae1fe405da41b6e9a80b607010d
karan-agarwal-01 <karanagarwal858@gmail.com>
2024-11-15 17:08:17 +0530

    shopping card added

---
+++ frontend/src/components/Shopping-Cart/data.js
@@ -0,0 +1,20 @@
+import jeans from './images/jeans.jfif'
+import Tshirt from './images/tshirt.webp'
+
+
+export const Items = [
+    {
+        id: '1',
+        name: 'White T-shirt',
+        price: '1000',
+        img: Tshirt,
+        quantity: 1,
+    },
+    {
+        id: '2',
+        name: 'Jeans',
+        price: '1200',
+        img: jeans,
+        quantity: 1,
+    },
+]
