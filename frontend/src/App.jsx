54ac5000668ae6d290c5c912cfee40c7397336b0
parthkaul-bit <127819564+parthkaul-bit@users.noreply.github.com>
2024-11-15 15:48:08 +0530

    created Navbar, Footer and auth components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "../src/components/Navbar/Navbar";
+import Footer from "../src/components/Footer/Footer";
+import SignupForm from "./components/SignupForm/SignupForm";
+import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSignup, setIsSignup] = useState(false);
 
-  const url = import.meta.env.VITE_PORT_URL
-
-  console.log(url)
+  const toggleForm = () => {
+    setIsSignup(!isSignup);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="dark:bg-gray-700 h-screen flex flex-col">
+      <Navbar />
+      <div className="flex flex-grow items-center justify-center p-4">
+        <div className="w-full max-w-4xl text-center px-4 md:px-0">
+          <span className="text-2xl text-gray-800 dark:text-white font-semibold">
+            {isSignup ? "Sign Up" : "Login"}
+          </span>
+          <div className="mt-4 flex justify-center items-center w-full">
+            {/* Conditional rendering of Signup or Login form */}
+            {isSignup ? <SignupForm /> : <LoginForm />}
+          </div>
+          <div className="flex justify-center mt-4">
+            <span
+              onClick={toggleForm}
+              className="px-4 text-gray-600 hover:underline rounded cursor-pointer dark:text-white"
+            >
+              {isSignup
+                ? "Already have an account? Log In"
+                : "Don't have an account? Sign Up"}
+            </span>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
