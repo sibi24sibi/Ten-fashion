7c2eed051268493aff5286fa5dfd4cb74f9e9173
Dheeraj-485 <dheerajangural781@gmail.com>
2024-11-15 08:49:52 +0530

    Backend of ContactPage

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const contactRoute=require("./routes/ContactRoute")
 
 const app = express();
+app.use(express.json());
+
+
+mongoose.connect("mongodb://localhost:27017/tenFashion",{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log("DB connected"))
+.catch((err)=>console.log("error connecting to db",err)
+
+)
+
+app.use("/api",contactRoute)
 
 require("dotenv").config({ path: "../.env" });
 
