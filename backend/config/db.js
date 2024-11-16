ef3c1150fcbcfde04c4bae2b3abe8ee171e0cfa7
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2024-11-16 14:34:20 +0530

    added product listing

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async (connectionUrl) => {
     console.log("Connected to MongoDB : " + connectionUrl);
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
-    process.exit(1); // Exit the process with failure
+    process.exit(1);
   }
 };
 
--- backend/controllers/productContrroller.js
+++ backend/controllers/productContrroller.js
@@ -1,6 +1,5 @@
 const Product = require("../models/product");
 
-// Create a new product
 const createProduct = async (req, res) => {
   try {
     if (!req.file) {
@@ -18,7 +17,7 @@ const createProduct = async (req, res) => {
 
     res.status(201).json(createdProducts);
   } catch (error) {
-    console.error(error); // Log the error to understand the issue
+    console.error(error);
     res.status(500).json({ message: "Error saving product", error });
   }
 };
@@ -33,18 +32,6 @@ const getProducts = async (req, res) => {
   }
 };
 
-// controllers/uploadController.js
-const uploadImage = (req, res) => {
-  if (req.file) {
-    res.status(200).json({
-      message: "Image uploaded successfully",
-      url: req.file.path,
-    });
-  } else {
-    res.status(400).json({ error: "Image upload failed" });
-  }
-};
-
 module.exports = {};
 
-module.exports = { createProduct, getProducts, uploadImage };
+module.exports = { createProduct, getProducts };
