fd034a1c15cdab132579c40bc910c834bea16cac
prashantbhoite123 <pbhoite985@gmail.com>
2024-11-20 16:39:55 +0530

    modify auth backend controller

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,34 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db.js");
-const productRoutes = require("./routes/productRoutes.js");
-const contactRoute = require("./routes/ContactRoute.js");
-const authRoutes = require("./routes/authRoutes.js");
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db.js")
+const productRoutes = require("./routes/productRoutes.js")
+const contactRoute = require("./routes/ContactRoute.js")
+const authRoutes = require("./routes/authRoutes.js")
+const cookieParser = require("cookie-parser")
 // Load environment variables
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: "../.env" })
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express()
+const PORT = process.env.PORT || 8000
 const CONNECTION_URL =
-  process.env.CONNECTION_URL || "mongodb://localhost:27017/tenFashion";
+  process.env.CONNECTION_URL || "mongodb://localhost:27017/tenFashion"
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+app.use(cookieParser())
 
 // Database connection
-connectDB(CONNECTION_URL);
+connectDB(CONNECTION_URL)
 
 // Routes
-app.use("/", productRoutes);
-app.use("/", contactRoute);
-app.use("/api/auth", authRoutes);
+app.use("/", productRoutes)
+app.use("/", contactRoute)
+app.use("/api/auth", authRoutes)
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
