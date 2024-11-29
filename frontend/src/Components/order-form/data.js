59dc6844b09bcb2e542d9850b64e34fd49663e00
karan-agarwal-01 <karanagarwal858@gmail.com>
2024-11-29 19:53:33 +0530

    order form added

---
+++ frontend/src/Components/order-form/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id: '1',
+        title: 'Retro plus velvet thick women jeans',
+        quantity: '1',
+        price: '1500',
+    },
+    {
+        id: '2',
+        title: 'Retro plus georget thick women suit',
+        quantity: '2',
+        price: '1900',
+    },
+]
